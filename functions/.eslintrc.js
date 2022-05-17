6c4673054c5bf8380ea5d9f8a32821a516596008
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-05-17 16:11:41 -0400

    fix(eslint): Specify correct root dir to tsconfig files

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["functions/tsconfig.json", "functions/tsconfig.dev.json"],
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    tsconfigRootDir: __dirname,
     sourceType: "module",
   },
   ignorePatterns: [
